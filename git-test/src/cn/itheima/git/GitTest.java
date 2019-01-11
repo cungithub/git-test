55304d48d07de3e0ce57394fe8161b08df7eea24
lcc <lcc@itheima.cn>
2019-01-11 14:21:59 +0800

    创建测试

---
+++ git-test/src/cn/itheima/git/GitTest.java
@@ -0,0 +1,13 @@
+package cn.itheima.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("五无哈符合客户罚款");
+        System.out.println("五无哈符合客户罚款");
+        System.out.println("五无哈符合客户罚款");
+        System.out.println("五无哈符合客户罚款");
+        System.out.println("五无哈符合客户罚款");
+        System.out.println("五无哈符合客户罚款");
+        System.out.println("五无哈符合客户罚款");
+    }
+}
